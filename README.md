35ce0339b5d06b6c6a7232b42dd2fe3372b51a75
Borislav Hadzhiev <opmftw@gmail.com>
2021-05-02 18:07:36 +0300

    added infra

---
+++ README.md
@@ -0,0 +1,29 @@
+# Add a Dynamodb Global Secondary Index in AWS CDK [GSI]
+
+A repository for an article on
+[bobbyhadz.com](https://bobbyhadz.com/blog/aws-cdk-add-global-secondary-index-dynamodb)
+
+## How to Use
+
+1. Clone the repository
+
+2. Install the dependencies
+
+```bash
+npm install
+```
+
+3. Create the CDK stack
+
+```bash
+npx cdk deploy
+```
+
+4. Open the AWS CloudFormation Console and the stack should be created in your
+   default region
+
+5. Cleanup
+
+```bash
+npx cdk destroy
+```
---
+++ lib/cdk-starter-stack.ts
@@ -0,0 +1,28 @@
+import * as dynamodb from '@aws-cdk/aws-dynamodb';
+import * as cdk from '@aws-cdk/core';
+
+export class CdkStarterStack extends cdk.Stack {
+  constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+    // 👇 create Dynamodb table
+    const table = new dynamodb.Table(this, id, {
+      partitionKey: {name: 'todoId', type: dynamodb.AttributeType.STRING},
+      sortKey: {name: 'createdAt', type: dynamodb.AttributeType.NUMBER},
+      billingMode: dynamodb.BillingMode.PROVISIONED,
+      readCapacity: 1,
+      writeCapacity: 1,
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+    });
+
+    // 👇 add global secondary index
+    table.addGlobalSecondaryIndex({
+      indexName: 'userIdIndex',
+      partitionKey: {name: 'userId', type: dynamodb.AttributeType.STRING},
+      sortKey: {name: 'status', type: dynamodb.AttributeType.STRING},
+      readCapacity: 1,
+      writeCapacity: 1,
+      projectionType: dynamodb.ProjectionType.ALL,
+    });
+  }
+}
